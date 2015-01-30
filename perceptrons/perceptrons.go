5ed47aadc08ad65ff29f0ea22eb6e9d4bf00b86c
Diego Borges <drborges.cic@gmail.com>
2015-01-30 01:06:29 -0200

    refactoring : creates perceptrons package

--- perceptrons/perceptrons.go
+++ perceptrons/perceptrons.go
@@ -1,4 +1,4 @@
-package neuralnets
+package perceptrons
 
 func Perceptron(inputs []float64, weights []float64, activation float64) int {
 	var sum = 0.0
@@ -13,22 +13,22 @@ func Perceptron(inputs []float64, weights []float64, activation float64) int {
 	return 0
 }
 
-func OrPerceptron(x1, x2 float64) int {
+func Or(x1, x2 float64) int {
 	activation := 1.0
 	return Perceptron([]float64{x1, x2}, []float64{1, 1}, activation)
 }
 
-func AndPerceptron(x1, x2 float64) int {
+func And(x1, x2 float64) int {
 	activation := 2.0
 	return Perceptron([]float64{x1, x2}, []float64{1, 1}, activation)
 }
 
-func XorPerceptron(x1, x2 float64) int {
+func Xor(x1, x2 float64) int {
 	activation := 1.0
-	and := float64(AndPerceptron(x1, x2))
+	and := float64(And(x1, x2))
 	return Perceptron([]float64{x1, x2, and}, []float64{1, 1, -2}, activation)
 }
 
-func NotPerceptron(x1 float64) int {
-	return XorPerceptron(x1, 1)
+func Not(x1 float64) int {
+	return Xor(x1, 1)
 }
---
+++ tests/perceptrons/perceptrons_test.go
@@ -0,0 +1,33 @@
+package perceptrons
+
+import (
+	"github.com/drborges/neuralnets/perceptrons"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestOrPerceptron(t *testing.T) {
+	assert.Equal(t, 1, perceptrons.Or(1, 1))
+	assert.Equal(t, 1, perceptrons.Or(1, 0))
+	assert.Equal(t, 1, perceptrons.Or(0, 1))
+	assert.Equal(t, 0, perceptrons.Or(0, 0))
+}
+
+func TestAndPerceptron(t *testing.T) {
+	assert.Equal(t, 1, perceptrons.And(1, 1))
+	assert.Equal(t, 0, perceptrons.And(1, 0))
+	assert.Equal(t, 0, perceptrons.And(0, 1))
+	assert.Equal(t, 0, perceptrons.And(0, 0))
+}
+
+func TestXorPerceptron(t *testing.T) {
+	assert.Equal(t, 0, perceptrons.Xor(1, 1))
+	assert.Equal(t, 1, perceptrons.Xor(1, 0))
+	assert.Equal(t, 1, perceptrons.Xor(0, 1))
+	assert.Equal(t, 0, perceptrons.Xor(0, 0))
+}
+
+func TestNotPerceptron(t *testing.T) {
+	assert.Equal(t, 0, perceptrons.Not(1))
+	assert.Equal(t, 1, perceptrons.Not(0))
+}
